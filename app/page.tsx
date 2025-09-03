fd476721e615d9e4a64ffbb33073cdf071c4fcc1
DimitriTedom <dimitritedom@gmail.com>
2025-09-03 12:14:20 +0100

    feat:Home page and BrainCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,46 @@
-import BrainCard from '@/components/BrainCard'
-import { Button } from '@/components/ui/button'
+import BrainCard from "@/components/BrainCard";
+import BrainList from "@/components/BrainList";
+import Cta from "@/components/Cta";
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
   return (
-  <main>
-    <h1 className='text-2xl underline'>Popular Brains</h1>
+    <main>
+      <h1 className="text-2xl underline">Popular Brains</h1>
 
-    <section className='home-section'>
-      <BrainCard/>
-      <BrainCard/>
-      <BrainCard/>
-    </section>
-  </main>
-  )
-}
+      <section className="home-section">
+        <BrainCard
+          id="12"
+          name="neura the Brainy Explorer"
+          topic="neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <BrainCard
+          id="123"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <BrainCard
+          id="124"
+          name="Verba the Vocabulary Builer"
+          topic="Language"
+          subject="English Literature"
+          duration={15}
+          color="#bde7ff"
+        />
+      </section>
 
-export default Page
+      <section className="home-section">
+        <BrainList />
+        <Cta />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
---
+++ components/BrainList.tsx
@@ -0,0 +1,7 @@
+const BrainList = () => {
+  return (
+    <div>BrainList</div>
+  )
+}
+
+export default BrainList
