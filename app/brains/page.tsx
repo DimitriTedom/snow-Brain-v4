10e179b9caa0014b946534c6e0ab5c0b1bdf88b8
DimitriTedom <dimitritedom@gmail.com>
2025-09-08 15:26:56 +0100

    feat: suscess implementation of brain Library

--- app/brains/page.tsx
+++ app/brains/page.tsx
@@ -1,6 +1,32 @@
-const BrainLibrary = () => {
+import BrainCard from "@/components/BrainCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+import { getAllBrains } from "@/lib/actions/brains.actions";
+import { getSubjectColor } from "@/lib/utils";
+
+const BrainLibrary = async ({searchParams}:SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  
+
+  const brains = await getAllBrains({subject,topic})
   return (
-    <div>Agents Library</div>
+    <main>
+    <section className="flex justify-between gap-4 max-sm:flex-col">
+      <h1>Brain Library</h1>
+      <div className="flex gap-4">
+        <SearchInput/>
+        <SubjectFilter/>
+      </div>
+    </section>
+
+    <section className="companions-grid">
+    {brains.map((brain)=>(
+      <BrainCard key={brain.id} {...brain} color={getSubjectColor(brain.subject)}/>
+    ))}
+    </section>
+    </main>
   )
 }
 
--- lib/actions/brains.actions.ts
+++ lib/actions/brains.actions.ts
@@ -14,3 +14,24 @@ export const createBrain = async (formData: CreateCompanion) => {
   if (error || !data) throw new Error(error?.message || "Failed to create Brain");
   return data[0];
 };
+
+export const getAllBrains = async ({limit = 10, page = 1, subject, topic}:GetAllCompanions) => {
+    const supabase = createSupabaseClient();
+    let query = (await supabase).from('brains').select();
+
+    if (subject && topic) {
+        query = query.ilike('subject',`%${subject}%`).or(`topic.ilke.%${topic}%,name.ilike.%${topic}%`)
+    }else if (subject) {
+        query = query.ilike('subject',`%${subject}%`)
+    }else if (topic) {
+        query = query.or(`topic.ilike.%${subject}%,name.ilike.%${topic}%`)
+        
+    }
+
+    query = query.range((page-1)*limit, page*limit-1)
+
+    const {data:brains, error}= await query;
+    if(error) throw new Error(error.message);
+
+    return brains
+}
